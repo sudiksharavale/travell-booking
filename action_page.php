dc78305c3a2158055ad252c0ac0ce3f60438cb09
Sudiksha <sudiksharavle2839@gmail.com>
2022-10-06 22:14:25 +0530

    first commit

---
+++ action_page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="projectt.css" />
+    <link rel="stylesheet" type="text/js" href="projecttt.js" />
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+  </head>
+  <body style="background-image: url('taxi.jpg')">
+
+                
+                        <nav>
+                            <a  href="project.html"><i class="fa fa-fw fa-home"></i> Home</a>
+                                <a  href="hotelf.html"><span>&#127869;</span> HOTEL</a>
+                                <a  href="flights.html"><span>&#9992;</span>flights</a>
+                                <a class="active" href="cabs.html"><span>&#128659;</span>Cabs</a> 
+
+                                <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
+                                <a href="contancat.html"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
+                                <a onclick="document.getElementById('id01').style.display='block'"><i class="fa fa-fw fa-user"></i>  </a>
+                                <a onclick="document.getElementById('id02').style.display='block'"><i class="fa fa-fw fa-user"></i> Sign Up </a>
+                                </nav>
+                                <form class="form-inline" action="action_page.php">
+                                  <label for="email"></label>
+                                  <input type="email" id="email" placeholder="Enter place location" name="email">
+                                  <label for="pwd"></label>
+                                  <input type="password" id="pwd" placeholder="Age" name="pswd">
+                                  
+                                  <button type="submit">Submit</button>
+                                </form>
+  </body>
+</html>
---
+++ projecttt.js
@@ -0,0 +1,20 @@
+const slideIndex = 0;
+showSlides();
+function showSlides() {
+ let i;
+ let slides = document.getElementsByClassName("");
+ let dots = document.getElementsByClassName("");
+ for (i = 0; i < slides.length; i++) {
+ slides[i].style.display = "none ";
+ }
+ slideIndex++;
+ if (slideIndex > slides.length) {
+ slideIndex = 1
+ }
+ for (i = 0; i < dots.length; i++) {
+ dots[i].className = dots[i].className.replace(" active ", "");
+ }
+ slides[slideIndex - 1].style.display = "block ";
+ dots[slideIndex - 1].className += " active ";
+ setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
